b2d0204e394bb1fd54ae2bc7da7bded7d9794a4b
notalentgeek <mikael.pratama@gmail.com>
2025-03-21 14:59:36 +0700

    Refactored node_data.gd
    The next thing to do is to refactor the remaining files.

--- core/data/node_data.gd
+++ core/data/node_data.gd
@@ -2,37 +2,53 @@
 extends RefCounted
 class_name NodeData
 
-# Node identification
+# Properties - Node Identification
+var depth: int
 var name: String
 var type: String
-var depth: int
 
-# Node content
-var script_code: String
+# Properties - Node Content
 var properties: Dictionary
+var script_code: String
 var signals: Array
 
-# Export context
+# Properties - Export Context
+var enabled_setting_categories: Array
 var error_log: Array
 var project_settings: Array
-var enabled_setting_categories: Array
 var screenshot_path: String
 
-# Hierarchy
+# Properties - Hierarchy
 var children: Array
 
-func _init(p_name: String, p_class: String, p_depth: int, p_script: String = "",
-		   p_properties: Dictionary = {}, p_signals: Array = [],
-		   p_error_log: Array = [], p_project_settings: Array = [],
-		   p_enabled_setting_categories: Array = [], p_screenshot_path: String = ""):
+# Constructor
+func _init(
+	p_name: String,
+	p_class: String,
+	p_depth: int,
+	p_script: String = "",
+	p_properties: Dictionary = {},
+	p_signals: Array = [],
+	p_error_log: Array = [],
+	p_project_settings: Array = [],
+	p_enabled_setting_categories: Array = [],
+	p_screenshot_path: String = ""
+):
+	# Node Identification
+	depth = p_depth
 	name = p_name
 	type = p_class
-	depth = p_depth
-	script_code = p_script
+
+	# Node Content
 	properties = p_properties
+	script_code = p_script
 	signals = p_signals
+
+	# Export Context
+	enabled_setting_categories = p_enabled_setting_categories
 	error_log = p_error_log
 	project_settings = p_project_settings
-	enabled_setting_categories = p_enabled_setting_categories
 	screenshot_path = p_screenshot_path
+
+	# Initialize empty children array
 	children = []
